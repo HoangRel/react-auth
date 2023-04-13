1afb18fdb373a5a72b905592054a0fee7e2b2134
hore <akairosukina@gmail.com>
2023-04-14 00:33:13 +0700

    finished

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -39,5 +39,16 @@ export async function action({ request }) {
   }
 
   // soon: manage that token
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
+
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem("expiration", expiration.toISOString());
+
+  //
+
   return redirect("/");
 }
